0352caf36e9b0dee4339d23fc514c277bbbf7473
Kamil Kusy <kamil.kusy111@gmail.com>
2024-07-15 13:49:52 +0200

    Add database handlers

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Client } = require("pg");
 require("dotenv").config();
 
 const client = new Client({
-  connectionString: `postgresql://adminuser:${process.env.DB_PASSWORD}@terraform-20240713112257563300000001.c7wez4pf1jkr.eu-central-1.rds.amazonaws.com:5432/appDB`,
+  connectionString: `postgresql://adminuser:${process.env.DB_PASSWORD}@terraform-20240730181737426200000012.cnygkg2oqckm.eu-central-1.rds.amazonaws.com:5432/appDB`,
   ssl: {
     require: true,
     rejectUnauthorized: false,

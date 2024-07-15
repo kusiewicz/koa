488ebf79d458d761d011e40695b94a380a46c894
Kamil Kusy <kamil.kusy111@gmail.com>
2024-07-15 13:49:52 +0200

    Add database handlers

--- router.js
+++ router.js
@@ -1,10 +1,31 @@
 const Router = require("koa-router");
 const router = new Router();
+const client = require("./db");
 
 router.get("/", (ctx) => {
   const var1 = process.env.VAR1;
   const var2 = process.env.VAR__2;
   ctx.body = `var1: ${var1} var2: ${var2}!`;
+});
+
+router.post("/add/:nickname", async (ctx) => {
+  const nickname = ctx.params.nickname;
+
+  const email = `${nickname}@gmail.com`;
+
+  const query = {
+    text: "INSERT INTO users(name, email) VALUES($1, $2) RETURNING id",
+    values: [nickname, email],
+  };
+  const { rows } = await client.query(query);
+
+  ctx.status = 201;
+  ctx.body = { id: rows[0].id, message: "User added successfully" };
+});
+
+router.get("/users", async (ctx) => {
+  const { rows } = await client.query("SELECT * FROM users");
+  ctx.body = rows;
 });
 
 module.exports = router;

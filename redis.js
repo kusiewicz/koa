f30a9c79a7ce471e2868efa7af694ece74dca9a4
Kamil Kusy <kamil.kusy111@gmail.com>
2024-08-06 13:47:10 +0200

    Add redis

---
+++ redis.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const Redis = require("ioredis");
+
+const redisClient = new Redis({
+  host: process.env.REDIS_HOST,
+  port: 6379,
+});
+
+redisClient.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+redisClient.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+module.exports = redisClient;

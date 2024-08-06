8d9188b99a59e34038eda0764e4d659a5ee50314
Kamil Kusy <kamil.kusy111@gmail.com>
2024-08-06 13:47:10 +0200

    Add redis

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const Router = require("koa-router");
 const router = new Router();
 const client = require("./db");
+const redisClient = require("./redis");
 
 router.get("/", (ctx) => {
   const var1 = process.env.VAR1;
@@ -10,7 +11,6 @@ router.get("/", (ctx) => {
 
 router.post("/add/:nickname", async (ctx) => {
   const nickname = ctx.params.nickname;
-
   const email = `${nickname}@gmail.com`;
 
   const query = {
@@ -19,6 +19,11 @@ router.post("/add/:nickname", async (ctx) => {
   };
   const { rows } = await client.query(query);
 
+  await redisClient.set(
+    `user:${rows[0].id}`,
+    JSON.stringify({ nickname, email })
+  );
+
   ctx.status = 201;
   ctx.body = { id: rows[0].id, message: "User added successfully" };
 });
@@ -26,6 +31,12 @@ router.post("/add/:nickname", async (ctx) => {
 router.get("/users", async (ctx) => {
   const { rows } = await client.query("SELECT * FROM users");
   ctx.body = rows;
+});
+
+router.get("/redis/:key", async (ctx) => {
+  const key = ctx.params.key;
+  const value = await redisClient.get(key);
+  ctx.body = { key, value };
 });
 
 module.exports = router;
